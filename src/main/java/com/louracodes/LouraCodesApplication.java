454b5a997ea8246b5707117c9db06a18da655ab0
louragini <medlouragini@gmail.com>
2023-11-29 22:41:58 +0100

    first commit

---
+++ src/main/java/com/louracodes/LouraCodesApplication.java
@@ -0,0 +1,13 @@
+package com.louracodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouraCodesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LouraCodesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/louracodes/LouraCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louracodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LouraCodesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
